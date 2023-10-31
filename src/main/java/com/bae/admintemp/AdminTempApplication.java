f6e3dd3562e01398350682f7c38a123496134fb2
baedj <qoehdwns>
2023-10-31 09:47:39 +0900

    첫 프로젝트

---
+++ src/main/java/com/bae/admintemp/AdminTempApplication.java
@@ -0,0 +1,13 @@
+package com.bae.admintemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminTempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bae/admintemp/AdminTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.admintemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminTempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
